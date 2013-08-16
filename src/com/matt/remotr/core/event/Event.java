69e8263c4fc459bb5be93155c9f2ef39416106c2
Matt Mackay <mattem@gmail.com>
2013-08-16 18:56:28 +0100

    Removed wrapped object from event

--- src/com/matt/remotr/core/event/Event.java
+++ src/com/matt/remotr/core/event/Event.java
@@ -1,8 +1,5 @@
 package com.matt.remotr.core.event;
 
-/**
- * Provides a base class on which to build different types of events
- */
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -12,7 +9,6 @@ public class Event {
 	private int id;
 	private String name;
 	private EventType eventType;
-	private Object wrappedPayload;
 	
 	@XmlElement(name="ID")
 	public int getId() {
@@ -27,15 +23,6 @@ public class Event {
 	@XmlElement(name="Type")
 	public EventType getEventType() {
 		return eventType;
-	}
-
-	@XmlElement(name="EventData")
-	public Object getWrappedPayload() {
-		return wrappedPayload;
-	}
-
-	public void setWrappedPayload(Object wrappedPayload) {
-		this.wrappedPayload = wrappedPayload;
 	}
 
 	public void setEventType(EventType eventType) {
