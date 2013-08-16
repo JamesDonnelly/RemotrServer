ef4ac2fa3c6737d4bf0e0b5fa0683675beb70724
Matt Mackay <mattem@gmail.com>
2013-08-16 18:57:22 +0100

    Better init methods

--- src/com/matt/remotr/core/job/CommandJob.java
+++ src/com/matt/remotr/core/job/CommandJob.java
@@ -1,14 +1,14 @@
 package com.matt.remotr.core.job;
 
+import org.jboss.logging.Logger;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.JobKey;
+import org.quartz.PersistJobDataAfterExecution;
 
 import com.matt.remotr.core.command.Command;
 import com.matt.remotr.core.device.Device;
-import com.matt.remotr.core.event.EventForwarder;
 
 /**
  * An implementation of the Quartz Job interface for running commands that run on devices.
@@ -16,16 +16,41 @@ import com.matt.remotr.core.event.EventForwarder;
  * @author mattm
  *
  */
-public class CommandJob implements Job {
-	private EventForwarder eventForwarder;
+@PersistJobDataAfterExecution
+public class CommandJob extends RemotrJob implements Job {
+	
+	private Logger log;
+	private JobDataMap dataMap;
+	private Device device;
+	private CommandForwarder commandForwarder;
+	
+	private void init(JobExecutionContext context){
+		log = Logger.getLogger(this.getClass());
+		
+		dataMap = context.getJobDetail().getJobDataMap();
+		
+		jobKey = context.getJobDetail().getKey();
+		command = (Command) dataMap.get("command");
+		jobName = (String) dataMap.get("jobname");
+		jobStatus = (JobStatus) dataMap.get("jobstatus");
+		cronExpression = (String) dataMap.get("cronexpression");
+		
+		device = (Device) dataMap.get("device");
+		commandForwarder = (CommandForwarder) dataMap.get("commandforwarder");
+	}
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+		init(context);
 		
-		Command command = (Command) dataMap.get("command");
-		Device device = (Device) dataMap.get("device");
+		log.debug("JobKey = ["+jobKey.toString()+"]");		
+		log.debug("Got device ["+device.getName()+"] for job");
+		log.debug("Got command ["+command.getName()+"] for job");
+		log.info("Starting job ["+jobName+"]");
 		
+		commandForwarder.forwardJob(device, this);
+		jobStatus = JobStatus.EXECUTING;
+		dataMap.put("jobstatus", jobStatus);
 	}
 
 }
