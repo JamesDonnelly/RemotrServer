f3a294976092dd44fde37bfb6a7a81952c2edd78
Matt Mackay <mattem@gmail.com>
2013-08-18 15:30:35 +0100

    Added tests to build files

---
+++ test/com/matt/remotr/core/device/DeviceCoordinatorWsImplTest.java
@@ -0,0 +1,52 @@
+package com.matt.remotr.core.device;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeviceCoordinatorWsImplTest {
+
+	public DeviceCoordinatorWsImplTest() {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDeviceCoordinatorWsImpl() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testRegister() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testDeregister() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testCheckRegistered() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetDeviceById() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetAllRegisteredDevices() {
+		assertTrue(true);
+	}
+
+}
