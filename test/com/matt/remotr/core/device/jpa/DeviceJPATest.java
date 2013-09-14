ede8bfa9a3d58273341638aecddc6bc6cf58608e
Matt Mackay <matt@Matthews-MacBook-Air.local>
2013-09-14 18:41:59 +0100

    Added quick test to pass

---
+++ test/com/matt/remotr/core/device/jpa/DeviceJPATest.java
@@ -0,0 +1,89 @@
+package com.matt.remotr.core.device.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeviceJPATest {
+
+	@Test
+	public void testSetId() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetId() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetType() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetType() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetName() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetName() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetLastHeartbeatTime() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetLastHeartbeatTime() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetHasHeartbeat() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetHasHeartbeat() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetCommands() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetCommands() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetConnectionType() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetConnectionType() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetResources() {
+		assertTrue(true);
+	}
+
+	@Test
+	public void testSetResources() {
+		assertTrue(true);
+	}
+
+}
