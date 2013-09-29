308967c7f76e267f98b5ecf13f8b042a93d9650a
Matt Mackay <matt@Matthews-MacBook-Air.local>
2013-09-29 23:05:26 +0100

    Refectored over to RemotrUtil

--- src/com/matt/remotr/core/job/JobStatus.java
+++
@@ -1,9 +0,0 @@
-package com.matt.remotr.core.job;
-
-import javax.xml.bind.annotation.XmlEnum;
-
-@XmlEnum
-public enum JobStatus {
-	CREATED, SCHEDULED, EXECUTING, FINISHED, RECOVERING, INERROR, FAILED, UNKNOWN
-
-}
