97f7a730d22ea8b0cbc1088f2cc7f34be39b6c46
Matt Mackay <mattem@gmail.com>
2013-10-15 21:35:59 +0100

    Initial commit of WsClass

---
+++ src/com/remotr/subsystem/ws/WsClass.java
@@ -0,0 +1,26 @@
+package com.remotr.subsystem.ws;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.remotr.subsystem.ws.response.domain.WsSubsystemHolder;
+
+/**
+ * Annotation interface that can be used on classes containing {@link WsMethod}s. 
+ * Information set here will set on {@link WsSubsystemHolder}
+ * @author matt
+ *
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WsClass {
+	
+	/**
+	 * Description of the service class
+	 */
+	String description();
+
+}
