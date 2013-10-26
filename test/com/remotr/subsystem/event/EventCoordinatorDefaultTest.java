f6c80131a841ad3a5c976bbd40d03f08fa8d66e0
Matt Mackay <mattem@gmail.com>
2013-10-26 18:29:20 +0100

    New test

---
+++ test/com/remotr/subsystem/event/EventCoordinatorDefaultTest.java
@@ -0,0 +1,96 @@
+package com.remotr.subsystem.event;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.remotr.subsystem.ws.WsTestBase;
+
+public class EventCoordinatorDefaultTest extends WsTestBase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testEventCoordinatorDefault() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetDeviceCoordinator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetResourceCoordinator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetTcpWsSender() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetXmppWsSender() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetSubsystem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRegisterForEventsDeviceEventType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testRegisterForEventsEventReceiver() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetEvents() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCacheEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testForwardEventResourceInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testForwardEventEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testForwardEventEventDevice() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testForwardJob() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testForwardWsResponse() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
