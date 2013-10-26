ab8fab3b938543c4415406b7f584f19b0e125748
Matt Mackay <mattem@gmail.com>
2013-10-26 18:26:44 +0100

    Adds login as an exposed service

--- src/com/remotr/subsystem/session/SessionCoordinatorService.java
+++ src/com/remotr/subsystem/session/SessionCoordinatorService.java
@@ -1,9 +1,18 @@
 package com.remotr.subsystem.session;
 
+import javax.annotation.PostConstruct;
+
+import com.remotr.subsystem.device.domain.Device;
+import com.remotr.subsystem.session.domain.DeviceSession;
 import com.remotr.subsystem.ws.WsBase;
 import com.remotr.subsystem.ws.WsCoordinator;
 import com.remotr.subsystem.ws.WsRunner;
+import com.remotr.subsystem.ws.annotations.WsClass;
+import com.remotr.subsystem.ws.annotations.WsMethod;
+import com.remotr.subsystem.ws.annotations.WsParam;
+import com.remotr.subsystem.ws.response.domain.WsResponse;
 
+@WsClass(description = "Provides methods for logging in and out of Remotr using a Device")
 public class SessionCoordinatorService extends WsBase implements WsRunner {
 	private WsCoordinator wsCoordinator;
 	private SessionCoordinator sessionCoordinator;
@@ -11,6 +20,31 @@ public class SessionCoordinatorService extends WsBase implements WsRunner {
 	public SessionCoordinatorService(){
 		super();
 		subSystemName = "Session";
+	}
+	
+	@PostConstruct
+	public void init(){
+		wsCoordinator.register(this);
+	}
+	
+	@WsMethod(
+			isPublic=true,
+			description="Login using an existing device - The device MUST already be registered",
+			wsParams = { 
+					@WsParam(name="device", type=Device.class)
+			})
+	public WsResponse login(Device device){
+		log.debug("Running login from Session wsService");
+		WsResponse response = getWsResponse();
+		DeviceSession deviceSession = sessionCoordinator.login(device);
+		
+		if(deviceSession != null){
+			log.debug("Setting DeviceSession as response object");
+			response.setResponse(deviceSession);
+			response.setSuccess(true);
+		}
+		
+		return response;
 	}
 	
 	public void setWsCoordinator(WsCoordinator wsCoordinator) {
