c03f23a8d8cfba8ebeb4ffdb1909307d29116dee
Matt Mackay <mattem@gmail.com>
2013-11-06 15:56:36 +0000

    Updates Jdoc

--- src/com/remotr/subsystem/ws/WsRunner.java
+++ src/com/remotr/subsystem/ws/WsRunner.java
@@ -2,7 +2,7 @@ package com.remotr.subsystem.ws;
 
 
 /**
- * Classes can implement interface to allow running of methods via the XMPP, TCPWS or Restful interface.
+ * Classes can implement interface to allow running of methods via the XMPP, TCPWS, Web Socket or Restful interface.
  * Not all methods can be run in RESTful way.
  * @author mattm
  *
