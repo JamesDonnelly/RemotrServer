7474c57f891f0ba8b90e1d83a8b376e8c2e577f2
Matt Mackay <mattem@gmail.com>
2013-11-06 16:03:56 +0000

    Initial commit

---
+++ src/com/remotr/subsystem/ws/WsEndpointProvider.java
@@ -0,0 +1,10 @@
+package com.remotr.subsystem.ws;
+
+/**
+ * Interface for class to implement that provide an endpoint (or gateway) for communication that exposes the Remotr Webservice
+ * @author matt
+ *
+ */
+public interface WsEndpointProvider extends WsSender {
+
+}
